582d3bdd70b0f88bf5f73600516495479a163032
Abenezer Mulugeta <abenezermulugeta31@gmail.com>
2021-10-13 10:46:15 +0300

    Use usernames from token to welcome users: top right dropdown.

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -10,6 +10,7 @@ export class AuthService {
 
   baseUrl='https://localhost:44323/api/auth/';
   jwtHelper = new JwtHelperService();
+  decodedToken : any;
 
   constructor(private http: HttpClient) { }
 
@@ -20,6 +21,7 @@ export class AuthService {
           const user = response;
           if(user){
             localStorage.setItem('token', user.token);
+            this.decodedToken = this.jwtHelper.decodeToken(user.token);
           }
         })
       );
