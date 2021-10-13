394a4cb08138390246cd07750ffde30689e8c3a9
Abenezer Mulugeta <abenezermulugeta31@gmail.com>
2021-10-13 18:17:24 +0300

    Guarding routes to revoke access from unauthorized users.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,11 +3,26 @@ import { HomeComponent } from "./home/home.component";
 import { ListsComponent } from "./lists/lists.component";
 import { MemberListComponent } from "./member-list/member-list.component";
 import { MessagesComponent } from "./messages/messages.component";
+import { AuthGuard } from "./_guards/auth.guard";
 
 export const appRoutes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'members', component: MemberListComponent},
-  {path: 'messages', component: MessagesComponent},
-  {path: 'lists', component: ListsComponent},
-  {path: '**', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', component: HomeComponent},
+
+  // This is used to guard multiple routes in one path
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'members', component: MemberListComponent},
+      {path: 'messages', component: MessagesComponent},
+      {path: 'lists', component: ListsComponent},
+    ]
+  },
+
+  // This is how to guard single route
+  //{path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+
+  // This wildcard redirects user to the home page which is identified by ''.
+  {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
